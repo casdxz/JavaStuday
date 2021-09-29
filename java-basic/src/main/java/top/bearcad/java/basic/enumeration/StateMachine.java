e35c20fb1f8f0ab6b59d7f47b82703d86f75e2bc
casdxz <19851903253@163.com>
2021-09-29 23:23:33 +0800

    枚举相关知识

---
+++ java-basic/src/main/java/top/bearcad/java/basic/enumeration/StateMachine.java
@@ -0,0 +1,11 @@
+package top.bearcad.java.basic.enumeration;/**
+* @program: java-basic
+*
+* @description: 
+*
+* @author: bearcad
+*
+* @create: 2021-09-29 22:53
+*
+**/public class StateMachine {
+}
