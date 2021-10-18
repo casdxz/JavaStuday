d1bf41eedc3fe78db2c63f81b37b8fa6e991b57d
casdxz <19851903253@163.com>
2021-10-18 22:46:54 +0800

    day26private Object readResolve() {

---
+++ java-basic/src/main/java/top/bearcad/java/basic/serialize/Student.java
@@ -0,0 +1,62 @@
+package top.bearcad.java.basic.serialize;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/**
+ * @program: java-basic
+ * @description:
+ * @author: bearcad
+ * @create: 2021-10-18 21:01
+ **/
+public class Student implements Serializable {
+    private static final long serialVersionUID = -4392658638228508589L;
+    private String name;
+    private Integer age;
+    private Integer score;
+    private transient String passwd;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public  Integer getAge(){
+        return age;
+    }
+    public void setAge(Integer age ){
+        this.age = age;
+    }
+    public Integer getScore(){
+        return score;
+    }
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getPassword() {
+        return passwd;
+    }
+
+    public void setPassword(String password) {
+        this.passwd = password;
+    }
+    private void readObject(ObjectInputStream objectInputStream) throws IOException,ClassNotFoundException{
+        objectInputStream.defaultReadObject();
+        if (0 >score || 100 < score){
+            throw new IllegalArgumentException("学生分数只能在0到100之间！");
+        }
+    }
+    @Override
+    public String toString() {
+        return "Student:" + '\n' +
+                "name = " + this.name + '\n' +
+                "age = " + this.age + '\n' +
+                "score = " + this.score + '\n' +
+                "passwd = " + this.passwd + '\n'
+
+                ;
+    }
+}
---
+++ java-basic/src/main/java/top/bearcad/java/basic/serialize/Text.java
@@ -0,0 +1,36 @@
+package top.bearcad.java.basic.serialize;
+
+import java.io.*;
+
+/**
+ * @program: java-basic
+ * @description:
+ * @author: bearcad
+ * @create: 2021-10-18 22:35
+ **/
+public class Text {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+
+        ObjectOutputStream objectOutputStream =
+                new ObjectOutputStream(
+                        new FileOutputStream( new File("singleton.txt") )
+                );
+        // 将单例对象先序列化到文本文件singleton.txt中
+        objectOutputStream.writeObject( Singleton.getSingleton() );
+        objectOutputStream.close();
+
+        ObjectInputStream objectInputStream =
+                new ObjectInputStream(
+                        new FileInputStream( new File("singleton.txt") )
+                );
+        // 将文本文件singleton.txt中的对象反序列化为singleton1
+        Singleton singleton1 = (Singleton) objectInputStream.readObject();
+        objectInputStream.close();
+
+        Singleton singleton2 = Singleton.getSingleton();
+
+        // 运行结果打印 false
+        System.out.println( singleton1 == singleton2 );
+    }
+
+}
