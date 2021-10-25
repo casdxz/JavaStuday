cfdf7654dc769b07e1f336d377553c8ed6203ca6
casdxz <19851903253@163.com>
2021-10-26 00:30:41 +0800

    day33 系统托盘

--- jfx-basic/src/main/java/module-info.java
+++ jfx-basic/src/main/java/module-info.java
@@ -4,9 +4,14 @@ module top.bearcad.jfxbasic {
     requires com.github.oshi;
     requires fastjson;
 
+    requires java.desktop;
 
     opens top.bearcad.jfx.basic to javafx.fxml;
     exports top.bearcad.jfx.basic;
+
+    opens top.bearcad.jfx.basic.trayicon to javafx.fxml;
+    exports top.bearcad.jfx.basic.trayicon;
+
     exports top.bearcad.jfx.basic.chart;
     opens top.bearcad.jfx.basic.chart to javafx.fxml;
 }
